66a8234ec84eaf466a5636552fea6a0c715b0444
Leo <hello@untrol.io>
2018-10-01 19:57:50 +1300

    .

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -16,43 +16,128 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
 #include <Arduino.h>
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include "Wire.h"   
+#include <SPI.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+//----------------------RF LIB STUFF ----------------------------------
 #include <RH_RF95.h>
-#include <RHEncryptedDriver.h>
+#include "RHEncryptedDriver.h"
 #include <Speck.h>
 
-RH_RF95 rf95;     // Instanciate a LoRa driver
-Speck myCipher;   // Instanciate a Speck block ciphering
+RH_RF95 rf95;     // Instances a LoRa driver
+Speck myCipher;   // Instances a Speck block ciphering
 RHEncryptedDriver myDriver(rf95, myCipher); // Instantiate the driver with those two
 
+//--------------------------------------------------------
+
+#define LED       5
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Aloha Wireless";
+char pass[] = "aloha123";
+
+// Keep this flag not to re-sync on every reconnection
+bool isFirstConnect = true;
+
 float frequency = 868.0; // Change the frequency here. 
-unsigned char encryptkey[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; // The very secret key
-char HWMessage[] = "Hello World ! I'm happy if you can read me";
-uint8_t HWMessageLen;
+unsigned char encryptkey[16]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; // The very secret key 
+
+//Declare instances and functions
+
+Blynk timer;
+void initRF(float f = 868.0);
+void blinkLED(void);
+
+void initRF(float f)
+{
+  Serial.println();
+  Serial.println("LoRa Simple_Encrypted Server");
+  if (!rf95.init()) Serial.println("LoRa init failed");
+  // Setup ISM frequency
+  rf95.setFrequency(f);
+  // Setup Power,dBm
+  rf95.setTxPower(14);
+  myCipher.setKey(encryptkey, 16);
+  delay(4000);
+  Serial.println("Setup completed");
+}
+
+void blinkLED(void)
+{
+  //!digitalWrite(LED);
+}
+
+// This function will run every time Blynk connection is established
+BLYNK_CONNECTED() {
+  if (isFirstConnect) {
+    // Request Blynk server to re-send latest values for all pins
+    Blynk.syncAll();
+
+    // You can also update individual virtual pins like this:
+    //Blynk.syncVirtual(V0, V2);
+
+    isFirstConnect = false;
+  }
+
+  // Let's write your hardware uptime to Virtual Pin 2
+  int value = millis() / 1000;
+  Blynk.virtualWrite(V2, value);
+}
+
+BLYNK_WRITE(V0)
+{
+  // Use of syncAll() will cause this function to be called
+  // Parameter holds last slider value
+  int sliderValue0 = param.asInt();
+}
+
+BLYNK_WRITE(V2)
+{
+  // You'll get uptime value here as result of syncAll()
+  int uptime = param.asInt();
+}
 
 void setup()
 {
-  HWMessageLen = strlen(HWMessage);
-  Serial.begin(9600);
-  while (!Serial) ; // Wait for serial port to be available
-  Serial.println("LoRa Simple_Encrypted Client");
-  if (!rf95.init())
-    Serial.println("LoRa init failed");
-  // Setup ISM frequency
-  rf95.setFrequency(frequency);
-  // Setup Power,dBm
-  rf95.setTxPower(13);
-  myCipher.setKey(encryptkey, sizeof(encryptkey));
-  Serial.println("Waiting for radio to setup");
-  delay(1000);
-  Serial.println("Setup completed");
+  // Debug console
+  Serial.begin(115200);
+  timer.setInterval(800, blinkLED);
+
+  initRF(frequency);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 }
 
 void loop()
 {
-  uint8_t data[HWMessageLen+1] = {0};
-  for(uint8_t i = 0; i<= HWMessageLen; i++) data[i] = (uint8_t)HWMessage[i];
-  myDriver.send(data, sizeof(data)); // Send out ID + Sensor data to LoRa gateway
-  Serial.print("Sent: ");
-  Serial.println((char *)&data);
-  delay(4000);
+  Blynk.run();
+
+  if (myDriver.available()) {
+    // Should be a message for us now   
+    uint8_t buf[myDriver.maxMessageLength()];
+    uint8_t len = sizeof(buf);
+    if (myDriver.recv(buf, &len)) {
+      Serial.print("Received: ");
+      Serial.println((char *)&buf);
+    }
+    else
+    {
+        Serial.println("recv failed");
+    }
+  }
 }
